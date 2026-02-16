8b49d7fa8aa882ead42be49285c1e60b22bbcbb4
Marielle Harrell <marielleharrell@Mac.local>
2026-02-16 17:50:25 -0500

    call API once

--- src/services/WeatherFetcher.jsx
+++ src/services/WeatherFetcher.jsx
@@ -5,6 +5,7 @@ export default function WeatherFetcher({ query, onData, onLoading, onError }) {
   const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
 
   useEffect(() => {
+    const abortController = new AbortController();
     const fetchWeather = async () => {
       try {
         onLoading(true);
@@ -20,19 +21,23 @@ export default function WeatherFetcher({ query, onData, onLoading, onError }) {
         const q = query === "Current Location" ? "auto:ip" : query;
         const res = await fetch(
           `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${q}&days=1&aqi=no&alerts=no`,
+          { signal: abortController.signal },
         );
         if (!res.ok) throw new Error("Failed to fetch weather");
         const data = await res.json();
         onData(data);
         console.log(data);
       } catch (err) {
-        onError("Could not load weather.");
+        if (err.name !== "AbortError") {
+          onError(err.message || "Could not load weather.");
+        }
       } finally {
         onLoading(false);
       }
     };
 
     fetchWeather();
+    return () => abortController.abort();
   }, [query]);
 
   return null;
