44d1cefa425e8e4f13a73ba259bcb333781b43a7
Marielle Harrell <builtbymarielle@gmail.com>
2026-06-26 21:25:42 -0400

    clicking outside deletion modal closes the modal

--- src/components/Modals/DeleteLocationModal.jsx
+++ src/components/Modals/DeleteLocationModal.jsx
@@ -3,22 +3,53 @@
  */
 
 import { isoAbbreviation } from "../../utils/uiHelpers";
+import { useEffect, useRef, useState } from "react";
 
 export default function DeleteLocationModal({
   locationToDelete,
   onCancel,
   onConfirm,
 }) {
+  const modalRef = useRef(null);
+  // remove the "show" class during animation out
+  const [isAnimateOut, setIsAnimateOut] = useState(false);
+  const animationClass = isAnimateOut ? "" : "show";
+
+  // Handle smooth closing animation when Canceling or clicking outside Modal
+  const handleCloseAnimation = () => {
+    setIsAnimateOut(true);
+    setTimeout(() => {
+      onCancel();
+    }, 150);
+  };
+
+  // Close the modal when clicking outside the modal-content box
+  useEffect(() => {
+    const handleOutsideClick = (e) => {
+      if (modalRef.current && modalRef.current.contains(e.target)) {
+        return;
+      }
+      handleCloseAnimation();
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => document.removeEventListener("mousedown", handleOutsideClick);
+  }, [onCancel]);
+
   if (!locationToDelete) return null;
 
   return (
     <>
-      <div className="modal fade show d-flex" tabIndex="-1">
-        <div className="modal-dialog modal-dialog-centered modal-slide-up">
+      <div 
+        className={`modal fade ${animationClass} d-block`} 
+        tabIndex="-1" 
+        role="dialog"
+      >
+        <div className="modal-dialog modal-dialog-centered modal-slide-up" ref={modalRef}>
           <div className="modal-content">
             <div className="modal-header">
               <h5 className="modal-title">Delete Location</h5>
-              <button type="button" className="btn-close" onClick={onCancel} />
+              <button type="button" className="btn-close" onClick={handleCloseAnimation} />
             </div>
 
             <div className="modal-body">
@@ -36,7 +67,7 @@ export default function DeleteLocationModal({
             </div>
 
             <div className="modal-footer">
-              <button className="btn btn-transparent" onClick={onCancel}>
+              <button className="btn btn-transparent" onClick={handleCloseAnimation}>
                 Cancel
               </button>
 
@@ -52,7 +83,7 @@ export default function DeleteLocationModal({
       </div>
 
       {/* backdrop */}
-      <div className="modal-backdrop fade show"></div>
+      <div className={`modal-backdrop fade ${animationClass}`}></div>
     </>
   );
 }
